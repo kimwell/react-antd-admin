82979998beefccc9464c91c101170d34dd89eb35
well kim <505144350@qq.com>
2020-07-21 16:35:14 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import store from './redux'
+import "antd/dist/antd.css";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/es/locale/zh_CN";
+import moment from "moment";
+import "moment/locale/zh-cn";
+moment.locale("zh-cn");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider
+      locale={zhCN}
+      componentSize="middle"
+      input={{ autoComplete: "off" }}
+    >
+      <Provider store={store}>
+        <div className="App">
+          <div>sdfsdf</div>
+        </div>
+      </Provider>
+    </ConfigProvider>
   );
 }
 
---
+++ src/hooks/useDocumentTitle.js
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from "react";
+
+const useDocumentTitle = (title, retainOnUnmount = false) => {
+  const defaultTitle = useRef(document.title);
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
+  useEffect(() => {
+    return () => {
+      if (!retainOnUnmount) {
+        document.title = defaultTitle.current;
+      }
+    };
+  }, []);
+};
+export default useDocumentTitle;
